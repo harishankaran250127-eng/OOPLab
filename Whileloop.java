e10da6f8d6718b8d569c4a6c0c8b726e5b006b6b
harishankaran250127-eng <harishankaran.250127@cse.ritchennai.edu.in>
2026-08-21 11:08:07 +0530

    Whileloop.java

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+public class Whileloop 
+{
+	public static void main(String[] args)
+	{
+		int count=5;
+		while(count>0) 
+		{
+			System.out.println(count);
+			count-=1;
+		}
+		System.out.println("LET'S START");
+	}
+}
