e18ef6c8eafeb5e386fc6bd3aaede987756c22fd
harishankaran250127-eng <harishankaran.250127@cse.ritchennai.edu.in>
2026-08-22 16:22:59 +0530

    OddOrEven.java

---
+++ OddOrEven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenOrOdd
+{
+	public static void main(String[] args)
+	{
+		Scanner reader=new Scanner(System.in);
+		System.out.print("Enter a number:");
+		int num=reader.nextInt();
+		if(num%2==0)
+		{
+			System.out.println(num + " is an even number.");
+		}
+		else
+		{
+			System.out.println(num + "is a odd number.");
+		}
+		reader.close();
+	}
+}
