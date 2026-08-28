4b04184f1b086b4a489fbc799f65781f8678096f
harishankaran250127-eng <harishankaran.250127@cse.ritchennai.edu.in>
2026-08-28 21:12:27 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package Observation;
+
+public class Sum
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		int sum= a+b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("Sum="+sum);
+	}
+}
