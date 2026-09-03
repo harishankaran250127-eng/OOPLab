212db766210a9ea2e8be31370d4381c3ce493289
harishankaran250127-eng <harishankaran.250127@cse.ritchennai.edu.in>
2026-09-03 09:24:08 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,12 @@
+package Exp2;
+
+import java.util.Scanner;
+
+public class Number {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("You entered:"+n);
+	}
+}
