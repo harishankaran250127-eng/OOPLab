f15a7b83a100a0a0f60195c28be69a9237d3ab4b
harishankaran250127-eng <harishankaran.250127@cse.ritchennai.edu.in>
2026-09-14 01:08:03 +0530

    Add files via upload

---
+++ FiboCal.java
@@ -0,0 +1,12 @@
+package Exp3;
+
+public class FiboCal {
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int c=a+b;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
