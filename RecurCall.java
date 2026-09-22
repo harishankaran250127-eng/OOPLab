7a08dbae42c8b18eb988c7affb0fcc3b1ee4f00d
harishankaran250127-eng <harishankaran.250127@cse.ritchennai.edu.in>
2026-09-22 23:33:43 +0530

    Add files via upload

---
+++ RecurCall.java
@@ -0,0 +1,22 @@
+package Exp3;
+
+import java.util.Scanner;
+
+public class RecurCall {
+	static int count=0;
+	static int fib(int n) {
+		count++;
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+		return fib(n-1)+fib(n-2);
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Sequence:");
+		for(int i=0;i<=n;i++)
+			System.out.print(fib(i)+" ");
+	}
+}
