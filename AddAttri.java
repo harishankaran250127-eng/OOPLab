28455ecb5927b6a4610369df2384adfca34e2eba
harishankaran250127-eng <harishankaran.250127@cse.ritchennai.edu.in>
2026-09-25 21:37:51 +0530

    Add files via upload

---
+++ AddAttri.java
@@ -0,0 +1,15 @@
+package Exp4;
+
+class Student2{
+	String name;
+	int rollNumber;
+}
+public class AddAttri {
+	public static void main(String[] args) {
+		Student2 s=new Student2();
+		s.name="Arjun";
+		s.rollNumber=101;
+		System.out.println("Name:"+s.name);
+		System.out.println("Roll Number:"+s.rollNumber);
+	}
+}
